b91012665b63b9447d4ca978d5716de2dde2d5a3
Braxton Jett <113302027+Jett2009@users.noreply.github.com>
2023-05-24 23:00:16 -0400

    Update App.js

--- snack-or-booze/src/App.js
+++ snack-or-booze/src/App.js
@@ -9,45 +9,54 @@ import Menu from "./FoodMenu";
 import Snack from "./FoodItem";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [snacks, setSnacks] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [snacks, setSnacks] = useState([]);
 
-  useEffect(() => {
-    async function getSnacks() {
-      let snacks = await SnackOrBoozeApi.getSnacks();
-      setSnacks(snacks);
-      setIsLoading(false);
-    }
-    getSnacks();
-  }, []);
+	useEffect(() => {
+		async function getSnacks() {
+			let snacks = await SnackOrBoozeApi.getSnacks();
+			setSnacks(snacks);
+			setIsLoading(false);
+		}
+		getSnacks();
+	}, []);
 
-  if (isLoading) {
-    return <p>Loading &hellip;</p>;
-  }
+	useEffect(() => {
+		async function getDrinks() {
+			let drinks = await SnackOrBoozeApi.getDrinks();
+			setDrinks(drinks);
+			setIsLoading(false);
+		}
+		getDrinks();
+	}, []);
 
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <NavBar />
-        <main>
-          <Switch>
-            <Route exact path="/">
-              <Home snacks={snacks} />
-            </Route>
-            <Route exact path="/snacks">
-              <Menu snacks={snacks} title="Snacks" />
-            </Route>
-            <Route path="/snacks/:id">
-              <Snack items={snacks} cantFind="/snacks" />
-            </Route>
-            <Route>
-              <p>Hmmm. I can't seem to find what you want.</p>
-            </Route>
-          </Switch>
-        </main>
-      </BrowserRouter>
-    </div>
-  );
+	if (isLoading) {
+		return <p>Loading &hellip;</p>;
+	}
+
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<NavBar />
+				<main>
+					<Switch>
+						<Route exact path="/">
+							<Home snacks={snacks} />
+						</Route>
+						<Route exact path="/snacks">
+							<Menu snacks={snacks} title="Snacks" />
+						</Route>
+						<Route path="/snacks/:id">
+							<Snack items={snacks} cantFind="/snacks" />
+						</Route>
+						<Route>
+							<p>Hmmm. I can't seem to find what you want.</p>
+						</Route>
+					</Switch>
+				</main>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
